d9ed16da293e2eca4c667860387da9f1572dc855
Nabil Yermani <nabilyermani@yahoo.de>
2024-10-22 21:18:52 +0200

    Database problems fixed

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const pool = new Pool({
     host: process.env.PGHOST,
     database: process.env.PGDATABASE,
     password: process.env.PGPASSWORD,
-    port: process.env.PGPORT, 
+    port: process.env.PGPORT,
 });
 
 app.get('/tasks', async (req, res) => {
